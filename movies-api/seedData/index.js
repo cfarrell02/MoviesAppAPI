78d498d5e3fb06022cc71e53399e95e581499e01
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-12-27 16:01:44 +0000

    Getmovies uses new api

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -2,6 +2,7 @@ import userModel from '../api/users/userModel';
 import genreModel from '../api/genres/genreModel';
 import movieModel from '../api/movies/movieModel';
 import movies from './movies.js';
+import shows from './shows';
 import users from './users';
 import genres from './genres';
 import dotenv from 'dotenv';
@@ -39,6 +40,18 @@ if (process.env.SEED_DB) {
 }
 
 export async function loadMovies() {
+  console.log('load seed data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
+  }
+}
+
+export async function loadShows() {
   console.log('load seed data');
   console.log(movies.length);
   try {
